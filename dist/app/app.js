e617b376fc2295dfa85acbc7b2dc900af93da958
Mehebul02 <mehebul2123@gmail.com>
2024-11-15 22:30:35 +0600

    add the error handle

--- dist/app/app.js
+++ dist/app/app.js
@@ -6,14 +6,53 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.get('/', (req, res) => {
-    console.log(req.query);
-    res.send('I am serving a bdCalling It Ltd as a front end developer');
+app.use(express_1.default.text());
+// router 
+const userRouter = express_1.default.Router();
+const createCourse = express_1.default.Router();
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/courses', createCourse);
+userRouter.get('/create-user', (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: 'Successfully data pass',
+        data: user
+    });
 });
+createCourse.post('/create-course', (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: 'successfully create the course',
+        data: course
+    });
+});
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+// app.get('/', logger, (req: Request, res: Response) => {
+//   res.send('I am serving a bdCalling It Ltd as a front end developer');
+// });
 app.post('/', (req, res) => {
     console.log(req.body);
     res.json({
         message: 'Transfer data'
     });
+});
+app.get('/', (req, res) => {
+    try {
+        res.send(something);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(400).json({
+            success: false,
+            message: 'failed to get data'
+        });
+    }
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -4,14 +4,54 @@ const app = express();
 app.use(express.json())
 app.use(express.text())
 
+// router 
+
+const userRouter = express.Router()
+
+const createCourse = express.Router()
+
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/courses', createCourse)
+
+userRouter.get('/create-user', (req: Request, res: Response) => {
+  const user = req.body
+  console.log(user);
+  res.json({
+    success: true,
+    message: 'Successfully data pass',
+    data: user
+  })
+})
+
+
+createCourse.post('/create-course', (req: Request, res: Response) => {
+  const course = req.body;
+  console.log(course);
+
+  res.json({
+    success: true,
+    message: 'successfully create the course',
+    data: course
+  })
+})
+
 const logger = (req: Request, res: Response, next: NextFunction) => {
   console.log(req.url, req.method, req.hostname);
+  next()
 }
 
+// error handle
 app.get('/', (req: Request, res: Response) => {
-  console.log(req.query);
-  res.send('I am serving a bdCalling It Ltd as a front end developer');
-});
+  try {
+    res.send(something)
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      success: false,
+      message: 'failed to get data'
+    })
+  }
+})
 
 app.post('/', (req: Request, res: Response) => {
   console.log(req.body);
@@ -21,5 +61,8 @@ app.post('/', (req: Request, res: Response) => {
 
 
 })
+
+
+
 
 export default app;
